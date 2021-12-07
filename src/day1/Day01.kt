b037e8e5237117dde7a291dc37a7d4ab2216c368
Julian Riegraf (ext.) <Julian.Riegraf@partner.bmwgroup.com>
2021-12-07 22:07:20 +0100

    complete day 1

---
+++ src/day1/Day01.kt
@@ -0,0 +1,21 @@
+package day1;
+
+import readInput
+
+fun main() {
+
+    val testInput = readInput("day1/input.txt")
+    var numberIncreased = 0
+    var prevNumber = Int.MAX_VALUE
+
+    testInput.forEach { line ->
+        val currentNumber = line.trim().toInt()
+        if (prevNumber < currentNumber) {
+            numberIncreased++
+        }
+        prevNumber = currentNumber;
+    }
+
+    println("increased $numberIncreased times")
+    check(numberIncreased == 1342)
+}
