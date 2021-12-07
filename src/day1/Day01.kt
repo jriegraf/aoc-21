40fabf9a107a18aa5b0ad7d82813534bc2ca27f6
jriegraf <julianriegraf@web.de>
2021-12-07 22:57:28 +0100

    added part 2 of day 1

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -2,20 +2,53 @@ package day1
 
 import readInput
 
+const val WINDOW_SIZE: Int = 3
+
 fun main() {
 
-    val testInput = readInput("day1/input.txt")
-    var numberIncreased = 0
-    var prevNumber = Int.MAX_VALUE
+    val testInput = toIntArray(readInput("day1/input.txt"))
 
-    testInput.forEach { line ->
-        val currentNumber = line.trim().toInt()
+    val solutionPartOne = part1(testInput)
+    println("increased $solutionPartOne times")
+    check(solutionPartOne == 1342)
+
+    val solutionPartTwo = part2(testInput)
+    println("increased $solutionPartTwo times")
+    check(solutionPartOne == 1378)
+}
+
+private fun part1(testInput: IntArray): Int {
+    var prevNumber = Int.MAX_VALUE
+    var numberIncreased = 0
+
+    testInput.forEach { currentNumber ->
         if (prevNumber < currentNumber) {
             numberIncreased++
         }
         prevNumber = currentNumber
     }
+    return numberIncreased
+}
 
-    println("increased $numberIncreased times")
-    check(numberIncreased == 1342)
+private fun part2(input: IntArray): Int {
+    var prevNumber = Int.MAX_VALUE
+    var numberIncreased = 0
+
+    for (i in WINDOW_SIZE..input.size) {
+        val currentNumber = getWindowSum(i, input)
+        if (prevNumber < currentNumber) {
+            numberIncreased++
+        }
+        prevNumber = currentNumber
+    }
+    return numberIncreased
+}
+
+private fun toIntArray(strList: List<String>) = strList.map { s -> s.trim().toInt() }.toIntArray()
+
+private fun getWindowSum(index: Int, array: IntArray): Int {
+    if (index < WINDOW_SIZE) throw IllegalArgumentException("index must be greater than window size")
+    if (array.size < WINDOW_SIZE) throw IllegalArgumentException("array length must be greater than window size")
+
+    return (index - WINDOW_SIZE until index).sumOf { array.get(it) }
 }
